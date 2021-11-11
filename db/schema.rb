d1deb29fa1db8fb0a5b0fac4d5a7bad4ed26097c
mahesh5131 <maheshmadesh141@gmail.com>
2021-11-11 21:51:56 +0530

    designing

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 2021_11_09_062059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "groceries", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_groceries_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_groceries_on_reset_password_token", unique: true
+  end
+
   create_table "lists", force: :cascade do |t|
     t.string "list_name"
     t.datetime "created_at", precision: 6, null: false
